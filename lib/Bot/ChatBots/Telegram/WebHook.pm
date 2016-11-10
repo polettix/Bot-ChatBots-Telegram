767d7d3f415ced2d525ceb745e1cb7ca96c2d7f5
Flavio Poletti <flavio@polettix.it>
2016-11-10 10:36:01 +0100

    Simplify WebHook with new tools

--- lib/Bot/ChatBots/Telegram/WebHook.pm
+++ lib/Bot/ChatBots/Telegram/WebHook.pm
@@ -1,117 +1,44 @@
 package Bot::ChatBots::Telegram::WebHook;
 use strict;
 use warnings;
-use Ouch;
 { our $VERSION = '0.001001'; }
 
-use Mojo::Base 'Bot::ChatBots::Telegram::Base';
+use Ouch;
+use Log::Any qw< $log >;
+use Data::Dumper;
 use Mojo::URL;
 use Mojo::Path;
-use Log::Any qw< $log >;
-use Try::Tiny;
 
-use Bot::ChatBots::Utils ();
+use Moo;
+use namespace::clean;
 
-has [qw< app _guard path url >];
+with 'Bot::ChatBots::Telegram::Role::Source'; # has normalize_record
+with 'Bot::ChatBots::Role::Source';
+with 'Bot::ChatBots::Role::WebHook';
 
-sub handler {
-   my ($self, $args) = @_;
+sub BUILD {
+   my $self = shift;
+   $self->install_route;
+}
 
-   my $source = {
-      app          => $self->app,
-      args         => $args,
-      object_token => $self->token,
-      processor    => $self->processor,
-      ref          => $self,
-      type         => $self->typename,
-   };
-
-   return sub {
-      my $c = shift;
-
-      # whatever happens, the bot "cannot" fail or Telegram will hammer
-      # us with the same update over and over
-      my ($outcome, $update);
-      try {
-         local $source->{controller} = $c;
-         $update = $c->req->json;
-         $outcome = $self->process(
-            {
-               source => $source,
-               stash  => $c->stash,
-               update => $update,
-            }
-         );
-         1;
-      } ## end try
-      catch {
-         $log->error("caught bot exception: $_");
-      };
-
-      if ($outcome) {    # give the outcome a try, please know what you do
-
-         # this is WebHook and Mojolicious specific, somehow
-         return if $outcome->{rendered};
-
-         # this is more generic, interpret as sendMessage by default
-         if (my $response = $outcome->{response}) {
-            $response = {text => $response} unless ref $response;
-            local $response->{method} = $response->{method}
-              // 'sendMessage';
-            local $response->{chat_id} = $response->{chat_id}
-              // $update->{message}{chat}{id};
-            return $c->render(json => $response);
-         }
-
-      } ## end if ($outcome)
-
-      # this is the safe approach - everything went fine, nothing to say
-      return $c->rendered(204);
-   };
-} ## end sub handler
-
-sub new {
-   my $package = shift;
-   my $self    = $package->SUPER::new(@_);
-
-   my $args = (@_ && ref($_[0])) ? $_[0] : {@_};
-
-   my $path = $self->path;
-   if (!defined($path)) {
-      my $url = $self->url
-        or ouch 500, 'undefined path and url for WebHook';
-      $path = Mojo::URL->new($url)->path->to_string;
-      $self->path($path);
-   } ## end if (!defined($path))
-
-   my $r = $args->{routes} // $self->app->routes;
-   my $route = $r->post($path => $self->handler);
-
-   $self->register() if $args->{register};
-
-   if ($args->{unregister}) {
-      my $token = $self->token;
-      $self->_guard(Bot::ChatBots::Utils::guard(sub { _register($token) }));
-   }
-
-   return $self;
-} ## end sub new
+sub parse_request {
+   my ($self, $req) = @_;
+   return $req->json;
+}
 
 sub register {
    my $self = shift;
    my $args = (@_ && ref($_[0])) ? $_[0] : {@_};
-   my $app  = $self->app;
 
-   my $token = $args->{token} // $self->token // ouch 500,
-     'Cannot register WebHook without a token';
+   my $app  = $args->{app} // $self->app;
+   my $token = $args->{token} // $self->token;
 
    my $wh_url;
    if (my $url = $args->{url} // $self->url) {
       $wh_url = Mojo::URL->new($url);
    }
    else {
-      my $path = $args->{path} // $self->path // ouch 500,
-        'Cannot register WebHook without a url or a path';
+      my $path = $args->{path} // $self->path;
       $path = Mojo::Path->new($path);
 
       my $c = $args->{controller} // $app->build_controller;
@@ -125,7 +52,7 @@ sub register {
       $form->{certificate} = $certificate;
    }
 
-   _register($args->{token} // $self->token, $form);
+   $self->_register($args->{token} // $self->token, $form);
 
    return $self;
 } ## end sub register
@@ -133,15 +60,15 @@ sub register {
 sub unregister {
    my $self = shift;
    my $args = (@_ && ref($_[0])) ? $_[0] : {@_};
-   _register($args->{token} // $self->token);
+   $self->_register($args->{token} // $self->token);
    return $self;
 } ## end sub unregister
 
 sub _register {
-   my ($token, $form) = @_;
+   my ($self, $token, $form) = @_;
    require WWW::Telegram::BotAPI;
    my $outcome = WWW::Telegram::BotAPI->new(token => $token)
-     ->setWebhook($form || {url => ''});
+     ->setWebhook($form // {url => ''});
    $log->info($outcome->{description} // 'unknown result');
    return;
 } ## end sub _register
