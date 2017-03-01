da688f82c09872d6a7b9a75867a064893559b352
Flavio Poletti <flavio@polettix.it>
2017-03-01 22:55:19 +0100

    0.006

--- lib/Bot/ChatBots/Telegram/Role/Source.pm
+++ lib/Bot/ChatBots/Telegram/Role/Source.pm
@@ -1,7 +1,7 @@
 package Bot::ChatBots::Telegram::Role::Source;
 use strict;
 use warnings;
-{ our $VERSION = '0.005001'; }
+{ our $VERSION = '0.006'; }
 
 use Ouch;
 use Log::Any;
--- lib/Bot/ChatBots/Telegram/Sender.pm
+++ lib/Bot/ChatBots/Telegram/Sender.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use Ouch;
 use 5.010;
-{ our $VERSION = '0.005001'; }
+{ our $VERSION = '0.006'; }
 
 use WWW::Telegram::BotAPI ();
 
